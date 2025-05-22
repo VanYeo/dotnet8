b00e7dd922c65cfa65d6974f0236589e834dd5d8
TPSSOFT\Vanessa.yeo <vanessa.yeo@tpssoft.com>
2025-05-22 18:26:23 +0700

    learn classes and constructors

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,118 +1,35 @@
-﻿Console.WriteLine("What do you want to do?");
-Console.WriteLine("[S]ee all todos");
-Console.WriteLine("[A]dd a todo");
-Console.WriteLine("[R]emove a todo");
-Console.WriteLine("[E]xit");
+﻿// constructor call taking year, month, date, time
+var IntPizzaDay = new DateTime(2023, 3, 14);
 
-// convert userinput to upper using ? if userInput isn't null
-var userInput = Console.ReadLine()?.ToUpper();
-List <string> ToDoList = new List<string>();
+var rectangle1 = new Rectangle(5, 10);
+class Rectangle
+{
+    // public access modifier allows global access
+    // public vars should start w capital letters
+    public int Width;
+    public int Height;
 
-while (userInput != "S" && userInput != "A" && userInput != "R" && userInput != "E")
-{
-    Console.WriteLine("Invalid input. Please enter S, A, R, or E.");
-    userInput = Console.ReadLine()?.ToUpper();
-}
-while (userInput != "E")
-{
-    Console.WriteLine("What do you want to do?");
-    userInput = Console.ReadLine()?.ToUpper();
-    if (userInput == "A")
+    // constructor that assigns values of params to fields
+    // must be named same as class name + no return type
+    // cannnot be called like a method in other contexts
+    public Rectangle(int width, int height)
     {
-        Console.WriteLine("Enter the TODO description:");
-        var description = Console.ReadLine();
-        while (ToDoList.Contains(description))
-        {
-            Console.WriteLine("The description must be unique.");
-            Console.WriteLine("Enter the TODO description:");
-            description = Console.ReadLine();
-        }
-        ToDoList.Add(description);
-        Console.WriteLine("TODO successfully added: " + description);
-        continue;
-    }
-    if (userInput == "S")
-    {
-        DisplayToDoList(ToDoList);
-        continue;
-    }
-    if (userInput == "R")
-    {
-        Console.WriteLine("Select the index of the TODO you want to remove"); 
-        DisplayToDoList(ToDoList);
-        var index = Console.ReadLine();
-        if (index != null && int.TryParse(index, out int indexInt) && indexInt > 0 && indexInt <= ToDoList.Count)
-        {
-            string removedTodo = ToDoList[indexInt - 1];
-            ToDoList.RemoveAt(indexInt - 1);
-            Console.WriteLine("TODO successfully removed: " + removedTodo);
-        }
-        else
-        {
-            Console.WriteLine("Invalid index. Please enter a valid number.");
-        }
+        Width = width;
+        Height = height;    
     }
 }
-Console.ReadKey();
 
-void DisplayToDoList(List<string> ToDoList)
+class HotelBooking
 {
-    Console.WriteLine("TODOs:");
-    for (int i = 0; i < ToDoList.Count; i++)
+    public string GuestName;
+    public DateTime StartDate;
+    public DateTime EndDate;
+
+    public HotelBooking(string guestName, DateTime startDate, int lengthOfStayInDays)
     {
-        Console.WriteLine($"{i + 1}. {ToDoList[i]}");
-    };
-}
-
-// or using switch
-
-while (userInput != "E")
-{
-    Console.WriteLine("What do you want to do?");
-    userInput = Console.ReadLine()?.ToUpper();
-
-    switch (userInput)
-    {
-        case "A":
-            Console.WriteLine("Enter the TODO description:");
-            var description = Console.ReadLine();
-            while (ToDoList.Contains(description))
-            {
-                Console.WriteLine("The description must be unique.");
-                Console.WriteLine("Enter the TODO description:");
-                description = Console.ReadLine();
-            }
-            ToDoList.Add(description);
-            Console.WriteLine("TODO successfully added: " + description);
-            break;
-
-        case "S":
-            DisplayToDoList(ToDoList);
-            break;
-
-        case "R":
-            Console.WriteLine("Select the index of the TODO you want to remove");
-            DisplayToDoList(ToDoList);
-            var index = Console.ReadLine();
-            if (index != null && int.TryParse(index, out int indexInt) && indexInt > 0 && indexInt <= ToDoList.Count)
-            {
-                string removedTodo = ToDoList[indexInt - 1];
-                ToDoList.RemoveAt(indexInt - 1);
-                Console.WriteLine("TODO successfully removed: " + removedTodo);
-            }
-            else
-            {
-                Console.WriteLine("Invalid index. Please enter a valid number.");
-            }
-            break;
-
-        case "E":
-            // Exit the loop
-            break;
-
-        default:
-            Console.WriteLine("Invalid input. Please enter S, A, R, or E.");
-            break;
+        GuestName = guestName;
+        StartDate = startDate;
+        EndDate = startDate.AddDays(lengthOfStayInDays);
     }
 }
-Console.ReadKey();
+
