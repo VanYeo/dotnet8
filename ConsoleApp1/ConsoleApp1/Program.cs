a65da028d71726457895ff8a9c30abd03b8b3a0a
TPSSOFT\Vanessa.yeo <vanessa.yeo@tpssoft.com>
2025-05-23 10:41:58 +0700

    learning publi, private, readonly classes and methods

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,6 +2,16 @@
 var IntPizzaDay = new DateTime(2023, 3, 14);
 
 var rectangle1 = new Rectangle(5, 10);
+var triangle1 = new Triangle(5, 10);
+var dog1 = new Dog("Max", 10, "Labrador");
+dog1.Describe();
+//Console.WriteLine(triangle1.AsString());
+
+
+//Console.WriteLine($"Width is {rectangle1.Width}");
+//Console.WriteLine($"Height is {rectangle1.Height}");
+//Console.WriteLine($"Area is {rectangle1.CalculateArea()}");
+//Console.WriteLine($"Area is {rectangle1.CalculateArea()}");
 class Rectangle
 {
     // public access modifier allows global access
@@ -16,6 +26,16 @@ class Rectangle
     {
         Width = width;
         Height = height;    
+    }
+    
+    public int CalculateCircumference()
+    {
+        return 2 * (Width + Height);
+    }
+
+    public int CalculateArea()
+    {
+        return Width * Height;
     }
 }
 
@@ -33,3 +53,55 @@ class HotelBooking
     }
 }
 
+class Triangle
+{
+    public int Base;
+
+    public int Height;
+    public Triangle(int @base, int height)
+    {
+        Base = @base;
+        Height = height;    
+    }
+
+    public double CalculateArea()
+    {
+        return (Base * Height) / 2; 
+    }
+
+    public string AsString()
+    {
+               return $"Base is {Base}, height is {Height}";
+    }
+}
+
+class Dog
+{
+    public string Name;
+    public int Weight;
+    public string Breed;
+    public Dog(string name, int weight, string breed)
+    {
+        Name = name;
+        Weight = weight;
+        Breed = breed;    
+    }
+    public Dog(string name, int weight)
+        : this(name, weight, "mixed-breed")
+    {
+    }
+    
+    public string WeightDescription()
+    {
+        return Weight switch
+        {
+            < 5 => "tiny",
+            > 5 and < 30 => "medium",
+            _ => "large"
+        };
+    }
+    public void Describe()
+    {
+        Console.WriteLine($"This dog is named {Name}, it's a {Breed}, and it weighs {Weight} kilograms, so it's a {WeightDescription()} dog");
+    }
+}
