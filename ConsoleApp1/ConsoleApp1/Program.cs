331bf0760369a339a51dc6bd086ddf74ed786eb1
TPSSOFT\Vanessa.yeo <vanessa.yeo@tpssoft.com>
2025-05-26 10:23:54 +0700

    learn inheritance from base constructor

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,45 +3,12 @@ using System.Drawing;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.Marshalling;
 
-var IntPizzaDay = new DateTime(2023, 3, 14);
-
-var rectangle1 = new Rectangle(5, 10);
-var triangle1 = new Triangle(5, 10);
-var dog1 = new Dog("Max", 10, "Labrador");
-dog1.Describe();
-
-Console.WriteLine(Calculator.Add(1, 3));
-
 var pizza = new Pizza();
 
-// Moved top-level statements before type declarations
-bool shallAddPositiveOnly = true;
-if (shallAddPositiveOnly)
-{
-    var numbers = new List<int> { 1, -2, 3, 4, -5 };
-    var calculator = new PositiveNumbersSumCalculator();
-    int positiveSum = calculator.Calculate(numbers);
-    Console.WriteLine($"Sum of positive numbers: {positiveSum}");
-}
+// calls constructor of base class first with argument of 12
+var cheddar = new Cheddar(2, 12);
 
-// Local function instead of invalid static method
-List<int> GetCountsOfAnimalsLegs()
-{
-    var animals = new List<Animal>
-    {
-        new Lion(),
-        new Tiger(),
-        new Duck(),
-        new Spider()
-    };
 
-    var result = new List<int>();
-    foreach (var animal in animals)
-    {
-        result.Add(animal.NumberOfLegs);
-    }
-    return result;
-}
 
 // Example usage of GetCountsOfAnimalsLegs
 var legsCounts = GetCountsOfAnimalsLegs();
@@ -65,6 +32,20 @@ public class Pizza
 // can be left empty if no shared properties/methods
 public class Ingredient
 {
+    public Ingredient(int priceIfExtraTopping
+    {
+        Console.WriteLine(
+            "Constructor from the Ingredient class");
+        priceIfExtraTopping = priceIfExtraTopping;
+    }
+
+    publix int PriceIfExtraTopping { get; }
+
+    public override string ToString() => ;
+
+    public virtual string Name { get; } = "Some ingredient";
+
+    public int PublicField; 
     // inherited by all ingredients
     public string PublicMethod() =>
         "This method is public in ingredient class";
@@ -77,7 +58,14 @@ public class Ingredient
 // inheritance: suggesting cheddar and tomato sauce are ingredients
 public class Cheddar : Ingredient
 {
-    public string Name => "Cheddar cheese";
+    public Cheddar(int PriceIfExtraTopping, int agedForMonths) : base(PriceIfExtraTopping)
+    {
+        AgedForMonths = agedForMonths;
+        Console.WriteLine("Constructor from the Cheddar class");
+    }
+
+    public int PriceIfExtraTopping { get; }
+    public override string Name => $"{base.Name}, more specifically, a cheddar cheese aged for {AgedForMonths}";
     public int AgedForMonths { get; }
 
     public void UseMethodsFromBaseClass()
@@ -90,53 +78,4 @@ public class TomatoSauce : Ingredient
 {
     public string Name => "Tomato sauce";
     public int TomatoesIn100Grams { get; }
-}
-
-public class PositiveNumbersSumCalculator
-{
-    public int Calculate(List<int> numbers)
-    {
-        int sum = 0;
-        foreach (var number in numbers)
-        {
-            if (ShallBeAdded(number))
-            {
-                sum += number;
-            }
-        }
-        return sum;
-    }
-
-    protected virtual bool ShallBeAdded(int number)
-    {
-        return number > 0;
-    }
-}
-
-public class Animal
-{
-    // Default number of legs is 4
-    public virtual int NumberOfLegs => 4;
-}
-
-public class Lion : Animal
-{
-    // Inherits NumberOfLegs = 4 from Animal
-}
-
-public class Tiger : Animal
-{
-    // Inherits NumberOfLegs = 4 from Animal
-}
-
-public class Duck : Animal
-{
-    // Override to return 2 legs
-    public override int NumberOfLegs => 2;
-}
-
-public class Spider : Animal
-{
-    // Override to return 8 legs
-    public override int NumberOfLegs => 8;
 }
