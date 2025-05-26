b915aaabc6d7801e8ded227301da972b284abde1
TPSSOFT\Vanessa.yeo <vanessa.yeo@tpssoft.com>
2025-05-26 14:35:23 +0700

    learn extension methods

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,17 +2,28 @@
 using System.Drawing;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.Marshalling;
+using System.Linq;
+using System.Collections.Generic;
 
 var pizza = new Pizza();
 
+var multiLineText = @"aaa
+bbb
+ccc
+ddd";
+
+int CountLines(string input) =>
+    input.Split(Environment.NewLine).Length;
+
+var ingredient = new Ingredient(5);
+
+// is operator checks if the object is of a specific type
+Console.WriteLine($"Is object: {ingredient is object}");
+Console.WriteLine($"Is object {ingredient is Ingredient}");
+Console.WriteLine($"Is object {ingredient is Cheddar}");
+
 // calls constructor of base class first with argument of 12
 var cheddar = new Cheddar(2, 12);
-
-
-
-// Example usage of GetCountsOfAnimalsLegs
-var legsCounts = GetCountsOfAnimalsLegs();
-Console.WriteLine("Animal legs counts: " + string.Join(", ", legsCounts));
 
 // Remove duplicate AddIngredient method from Pizza class
 
@@ -32,20 +43,23 @@ public class Pizza
 // can be left empty if no shared properties/methods
 public class Ingredient
 {
-    public Ingredient(int priceIfExtraTopping
+    public Ingredient(int priceIfExtraTopping)
     {
         Console.WriteLine(
             "Constructor from the Ingredient class");
-        priceIfExtraTopping = priceIfExtraTopping;
+        PriceIfExtraTopping = priceIfExtraTopping;
     }
 
-    publix int PriceIfExtraTopping { get; }
+    public int PriceIfExtraTopping { get; }
 
-    public override string ToString() => ;
+    public override string ToString() => Name;
 
     public virtual string Name { get; } = "Some ingredient";
 
-    public int PublicField; 
+    // no point adding method as will be overriden in derived classes
+    public abstract void Prepare();
+
+    public int PublicField;
     // inherited by all ingredients
     public string PublicMethod() =>
         "This method is public in ingredient class";
@@ -55,16 +69,23 @@ public class Ingredient
          "This method is private in ingredient class";
 }
 
-// inheritance: suggesting cheddar and tomato sauce are ingredients
-public class Cheddar : Ingredient
+public abstract class Cheese : Ingredient
 {
-    public Cheddar(int PriceIfExtraTopping, int agedForMonths) : base(PriceIfExtraTopping)
+    public Cheese(int priceIfExtraTopping) : base(priceIfExtraTopping)
+    {
+        Console.WriteLine("Constructor from the Cheese class");
+    }
+}
+
+// inheritance: suggesting cheddar and tomato sauce are ingredients
+public class Cheddar : Cheese
+{
+    public Cheddar(int priceIfExtraTopping, int agedForMonths) : base(priceIfExtraTopping)
     {
         AgedForMonths = agedForMonths;
         Console.WriteLine("Constructor from the Cheddar class");
     }
 
-    public int PriceIfExtraTopping { get; }
     public override string Name => $"{base.Name}, more specifically, a cheddar cheese aged for {AgedForMonths}";
     public int AgedForMonths { get; }
 
@@ -74,8 +95,85 @@ public class Cheddar : Ingredient
     }
 }
 
+// sealed class prevents further inheritance
+public sealed class Mozzarella : Cheese
+{
+    public Mozzarella(int priceIfExtraTopping, int waterContent) : base(priceIfExtraTopping)
+    {
+        
+    }
+    public override string Name => "Mozzarella";
+    public bool IsLight { get; }
+
+    public override void Prepare()
+    {
+        Console.WriteLine("Slice thinly and place on top of the pizza");
+    }
+}
 public class TomatoSauce : Ingredient
 {
-    public string Name => "Tomato sauce";
+    public TomatoSauce(int priceIfExtraTopping, int tomatoesIn100Grams)
+        : base(priceIfExtraTopping)
+    {
+        TomatoesIn100Grams = tomatoesIn100Grams;
+    }
+
+    public override string Name => "Tomato sauce";
     public int TomatoesIn100Grams { get; }
+}
+
+//your code goes here - define the Shape class
+public abstract class Shape
+{
+    public abstract double CalculateArea();
+}
+
+public class Square : Shape
+{
+    public double Side { get; }
+
+    public Square(double side)
+    {
+        Side = side;
+    }
+
+    //your code goes here
+    public override double CalculateArea()
+    {
+        return Side * Side;
+    }
+
+
+public class Rectangle : Shape
+{
+    public double Width { get; }
+    public double Height { get; }
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    //your code goes here
+    public override double CalculateArea()
+    {
+        return Width * Height;
+        }
+    }
+
+public class Circle : Shape
+{
+    public double Radius { get; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    //your code goes here'
+    public override double CalculateArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
 }
